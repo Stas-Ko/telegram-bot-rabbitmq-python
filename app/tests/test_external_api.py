c0afb9bf8d4318b514a0ec6228f2163fd059ca2c
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-12-01 01:06:33 +0200

    Create test_external_api.py

---
+++ app/tests/test_external_api.py
@@ -0,0 +1,17 @@
+import pytest
+import requests_mock
+from external_api import send_message_to_external_api
+
+def test_send_message_to_external_api():
+    # Мокаем запрос к внешнему API и проверяем, что сообщение успешно отправляется
+    with requests_mock.Mocker() as m:
+        url = '<external-api-url>'
+        m.post(url, status_code=200)
+
+        send_message_to_external_api('Hello, API')
+        assert m.called_once
+
+        request = m.request_history[0]
+        assert request.url == url
+        assert request.method == 'POST'
+        assert request.json() == {'message': 'Hello, API'}
